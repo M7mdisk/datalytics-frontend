fef3adaad37a42f58c649de55e6210a4f499fa58
Abdulrahman39 <abood.zaiem89@gmail.com>
2023-03-04 15:58:22 +0300

    new ML model page and ML page

--- src/views/NewMLModel.vue
+++ src/views/NewMLModel.vue
@@ -1,74 +1,130 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed, watch } from 'vue';
 
 import { axiosAPI } from '@/axiosAPI';
 
 const datasets = ref({})
 const datasetsName = ref([])
 const dropdownValue = ref('')
-const  showClo = ref(false)
+const showClo = ref(false)
 const datasetsCol = ref([])
-const selectedCol = ref('')
-const usedCols = ref('')
-const modelName = ref('') 
+const datasetsUsedCol = ref([])
+const selectedCol = ref([])
+const usedCols = ref([])
+const modelName = ref('')
+const showUsedClo = ref(false)
+
+const check = computed(() => {
+    return CheckData()
+})
+
+watch([modelName, selectedCol, usedCols], () => {
+
+})
+
 
 onMounted(() => {
-    axiosAPI.get('/datasets/').then((data)=>{
-        datasets.value=data.data
-        console.log(datasets.value[0].file_name)
+    axiosAPI.get('/datasets/').then((data) => {
+        datasets.value = data.data
         SetData(data.data)
     })
-}); 
+});
 
-function SetData(data){
+function CheckData() {
+    if (modelName.value == '') {
+        return false;
+    }
+    if (selectedCol.value.id == undefined) {
+        return false
+    }
+    if (usedCols.value.length == 0) {
+        return false
+    }
+    else {
+        console.log(selectedCol.value, usedCols.value)
+        return true
+    }
+}
+
+function SetData(data) {
     for (const key in data) {
         datasetsName.value.push(data[key].file_name)
     }
-    
-}
-function GetId(name){
- for (const key in datasets.value) {
-    if(datasets.value[key].file_name==name){
-        return datasets.value[key].id
-    }
-}}
 
-async function getDatasetDetails(){
+}
+function GetId(name) {
+    for (const key in datasets.value) {
+        if (datasets.value[key].file_name == name) {
+            return datasets.value[key].id
+        }
+    }
+}
+
+async function getDatasetDetails() {
     const id = GetId(dropdownValue.value)
-    axiosAPI.get(`/datasets/${id}`).then((data)=>{
+    axiosAPI.get(`/datasets/${id}`).then((data) => {
         datasetsCol.value = data.data.columns
-    console.log(datasetsCol.value)
     })
     showClo.value = true
+}
+function colUsedToPred() {
+    datasetsUsedCol.value = datasetsCol.value.filter(function (value, index, arr) {
+        return value !== selectedCol.value;
+    });
+    showUsedClo.value = true
+    usedCols.value= usedCols.value.filter(function (value, index, arr) {
+        return value !== selectedCol.value;
+    });
+
+}
+
+
+
+async function CreateModel() {
+    const usedcolID = []
+    for (const key in usedCols.value) {
+        console.log(usedCols.value[key])
+        usedcolID.push(usedCols.value[key].id)
+    }
+    const data = {
+        name: modelName.value,
+        dataset: GetId(dropdownValue.value),
+        target: selectedCol.value.id,
+        features:usedcolID
+    }
+    axiosAPI.post('/models/',data)
 }
 </script>
 <template>
     <h1>Create New Model:</h1>
     <div class="grid p-fluid">
         <div class="col-12">
-            <div class="card shadow-1" style="min-height: 800px">
+            <div class="card shadow-1" style="min-height: 700px">
                 <div class="grid">
                     <div class="col-12 md:col-6">
                         <h5>Model name</h5>
                         <div class="grid formgrid">
                             <div class="col-12 mb-4">
-                                <InputText type="text" v-model="modeleNmae"  placeholder="Model name"></InputText>
+                                <InputText type="text" v-model="modelName" placeholder="Model name"></InputText>
                             </div>
                         </div>
 
                         <h5 v-if="showClo">What do you want to predict?</h5>
                         <div v-if="showClo" class="grid formgrid">
                             <div class="col-12 mb-2">
-                                <Dropdown v-model="selectedCol" :options="datasetsCol" optionLabel="name" placeholder="Select" />
+                                <Dropdown v-model="selectedCol" :options="datasetsCol" optionLabel="name"
+                                    placeholder="Select" @change="colUsedToPred" />
                             </div>
                         </div>
 
-                        <h5 v-if="showClo">What fields do you want to use in the prediction?</h5>
-                        <div v-if="showClo" class="grid formgrid">
+                        <h5 v-if="showUsedClo">What fields do you want to use in the prediction?</h5>
+                        <div v-if="showUsedClo" class="grid formgrid">
                             <div class="col-12 mb-2">
-                                <MultiSelect v-model="usedCols" :options="datasetsCol" optionLabel="name" placeholder="Select Columns" :filter="true">
+                                <MultiSelect v-model="usedCols" :options="datasetsUsedCol" optionLabel="name"
+                                    placeholder="Select Columns" :filter="true">
                                     <template #value="slotProps">
-                                        <div class="inline-flex align-items-center py-1 px-2 bg-primary text-primary border-round mr-2" v-for="option of slotProps.value" :key="option.code">
+                                        <div class="inline-flex align-items-center py-1 px-2 bg-primary text-primary border-round mr-2"
+                                            v-for="option of slotProps.value" :key="option.code">
                                             <div>{{ option.name }}</div>
                                         </div>
                                         <template v-if="!slotProps.value || slotProps.value.length === 0">
@@ -81,8 +137,7 @@ async function getDatasetDetails(){
                                         </div>
                                     </template>
                                 </MultiSelect>
-                                <!-- <Listbox v-model="listboxValue" :multiple="true" :options="listboxValues"
-                                    optionLabel="name" :filter="true" /> -->
+
                             </div>
                         </div>
                     </div>
@@ -90,7 +145,8 @@ async function getDatasetDetails(){
                         <h5>Dataset</h5>
                         <div class="grid formgrid">
                             <div class="col-12 mb-2">
-                                <Dropdown v-model="dropdownValue" :options="datasetsName" @change="getDatasetDetails" placeholder="Select" />
+                                <Dropdown v-model="dropdownValue" :options="datasetsName" @change="getDatasetDetails"
+                                    placeholder="Select" />
                                 <RouterLink to="/datasets/upload">
                                     <p class="ml-2 text-primary">Create New Dataset</p>
                                 </RouterLink>
@@ -100,7 +156,8 @@ async function getDatasetDetails(){
                 </div>
                 <div class="col-12 h-22rem xs:h-3rem"></div>
                 <div class="flex mr-6 justify-content-end">
-                    <div><Button @click="upload" label="Create" style="left: 0; bottom: 0; position: relative" class="p-button-raised-rounded m-5 mr-2 mb-2 h-3rem" /></div>
+                    <div><Button @click="CreateModel" label="Create" style="left: 0; bottom: 0; position: relative"
+                            class="p-button-raised-rounded m-5 mr-2 mb-2 h-3rem" :disabled="!check" /></div>
                 </div>
             </div>
         </div>
