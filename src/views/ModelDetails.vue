fbcbdd53fb2383379961b963fc7800910443996c
Mohammad Iskandarani <M7md.k.isk@gmail.com>
2023-03-15 18:32:51 +0300

    Formatting

--- src/views/ModelDetails.vue
+++ src/views/ModelDetails.vue
@@ -4,20 +4,16 @@ import { ref, onMounted } from 'vue';
 import { axiosAPI } from '@/axiosAPI';
 const route = useRoute();
 const active = ref(0);
-const SelectedPage = ref('')
+const SelectedPage = ref('Analytics');
 const TabMenu = ref([
     {
-        label: 'Analytics',
-
+        label: 'Analytics'
     },
     {
-        label: 'Use and Deploy',
-    },
-
+        label: 'Use and Deploy'
+    }
 ]);
-const TabMenu2 = ref([
-    'Analytics', 'Use and Deploy'
-]);
+const TabMenu2 = ref(['Analytics', 'Use and Deploy']);
 const model = ref({});
 onMounted(() => {
     axiosAPI.get(`/models/${route.params.id}`).then((res) => {
@@ -42,7 +38,7 @@ function randomColor() {
     var blue = Math.floor(Math.random() * 256);
 
     // Create a CSS color string from the random values
-    var colorString = "rgb(" + red + ", " + green + ", " + blue + ")";
+    var colorString = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
 
     // Return the CSS color string
     return colorString;
@@ -58,13 +54,11 @@ function randomColor() {
             <TabMenu :model="TabMenu" />
             <SelectButton v-model="SelectedPage" :options="TabMenu2" aria-labelledby="basic" />
         </div>
-
     </div>
     <div>
         <div class="card flex gap-3 justify-content-between">
             <div class="flex gap-3">
-                <div><circle-progress :size="110" :percent="model.accuracy * 100 ? model.accuracy * 100 : 1"
-                        :show-percent="true" :viewport="true" /></div>
+                <div><circle-progress :size="110" :percent="model.accuracy * 100 ? model.accuracy * 100 : 1" :show-percent="true" :viewport="true" /></div>
                 <div>
                     <h3 class="mb-0">{{ model.name }}</h3>
                     <p class="text-color-secondary mb-1">
@@ -77,8 +71,7 @@ function randomColor() {
                             })
                         }}
                     </p>
-                    <div class="text-color-secondary flex align-items-center gap-1 justify-content center text-lg"
-                        v-if="model.model_type == 'C'">
+                    <div class="text-color-secondary flex align-items-center gap-1 justify-content center text-lg" v-if="model.model_type == 'C'">
                         <i class="pi pi-tags" style="font-size: 1.5rem"></i>
                         <p>Categorical</p>
                     </div>
@@ -92,149 +85,101 @@ function randomColor() {
                 </div>
             </div>
 
-            <div class="flex flex-column align-items-end justify-content-end">
-                <span class="font-bold">Dataset:</span>
-                <i class="pi pi-file-excel mx-1" style="color: green"></i>
-                <span class="fnt-bold">{{ ' ' + model.dataset }}</span>
-                <div class=" flex gap-3 justify-content-between">
+            <div class="flex flex-column align-items-end justify-content-end gap-3">
+                <div>
+                    <span class="font-bold">Dataset:</span>
+                    <i class="pi pi-file-excel mx-1" style="color: green"></i>
+                    <span class="fnt-bold">{{ ' ' + model.dataset }}</span>
+                </div>
+                <div class="flex gap-3 justify-content-between">
                     <div class="flex gap-3">
                         <!-- <div><circle-progress :size="110" :percent="model.accuracy * 100 != 0 ? model.accuracy * 100 : 1"
                                                                         :show-percent="true" :viewport="true" /></div> -->
                         <div>
-                            <h3 class="mb-0">{{ model.name }}</h3>
-                            <p class="text-color-secondary mb-1">
-                                {{
-                                    new Date(model.created_at).toLocaleString('en-GB', {
-                                        hour12: true,
-                                        timeZone: 'Asia/Kuwait',
-                                        timeStyle: 'short',
-                                        dateStyle: 'medium'
-                                    })
-                                }}
-                            </p>
                             <p>
-                                <span class="font-bold">Predicting column:</span><span class="fnt-bold">{{ ' ' +
-                                    model.target }}</span>
+                                <span class="font-bold">Predicting column:</span><span class="fnt-bold">{{ ' ' + model.target }}</span>
                             </p>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
 
         <!-- Top fields -->
-        <div class="card  ">
-            <h2>Top Fields</h2>
-            <p class="text-color-secondary mb-1 text-lg ml-4">Fields ranked by their contribution to the
-                prediction result
-            </p>
-            <div class="grid ml-5 mt-5 ">
+        <div class="card">
+            <h4 class="mb-1">Top Fields</h4>
+            <p class="text-color-secondary text-lg">Fields ranked by their contribution to the prediction result</p>
+            <div class="grid ml-1 mt-2 gap-2">
                 <div class="grid col-12">
-                    <div class="col-1 mt-1">
-                        <h4>Age</h4>
+                    <div class="col-1">
+                        <p class="text-lg">Age</p>
                     </div>
-                    <div class="col align-items-center mt-2">
-                        <ProgressBar class="text-color-secondary" :value="50"></ProgressBar>
+                    <div class="col align-items-center">
+                        <ProgressBar class="text-color-secondary px-0" :value="50"></ProgressBar>
                     </div>
-
                 </div>
-                <div class="grid col-12">
-                    <div class="col-1 mt-1">
-                        <h4>Age</h4>
-                    </div>
-                    <div class="col align-items-center mt-2">
-                        <ProgressBar class="text-color-secondary" :value="30"></ProgressBar>
-                    </div>
-
-                </div>
-                <div class="grid col-12">
-                    <div class="col-1 mt-1">
-                        <h4>Age</h4>
-                    </div>
-                    <div class="col align-items-center mt-2">
-                        <ProgressBar class="text-color-secondary" :value="70"></ProgressBar>
-                    </div>
-
-                </div>
-
-
             </div>
         </div>
         <!-- segments -->
 
-        <div class=" grid flex gap-3">
-            <div class="mb-1 ml-4">
-                <h2>Segments</h2>
-                <p class="text-color-secondary mb-1 text-lg ">Sets of similar records in your dataset, grouped by
-                    outcome of
-                    interest</p>
+        <div class="grid flex gap-3">
+            <div class="ml-4 mt-1">
+                <h4 class="mb-1">Segments:</h4>
+                <p class="text-color-secondary mb-1 text-lg">Sets of similar records in your dataset, grouped by the outcome of interest</p>
             </div>
 
-            <div class="grid col-12 ml-1 gap-3 ">
-                <div class="card col m-0 ">
-                    <div class="grid justify-content-between">
-                        <div class="col-1 mr-3">
-                            <Avatar :style="{ backgroundColor: randomColor(), color: randomColor() }" size="xlarge"
-                                shape="circle" icon="pi pi-star-fill" />
+            <div class="grid col-12 ml-1 gap-3">
+                <div class="card col m-0">
+                    <div class="grid justify-content-between gap-1">
+                        <div class="col-1">
+                            <Avatar :style="{ backgroundColor: randomColor(), color: randomColor() }" size="large" shape="circle" icon="pi pi-star-fill" />
                         </div>
-                        <div class="col m-2">
-                            <h5>Segement 1</h5>
-                            <p class="text-color-secondary   ">This segment has the highest likelihood of subscription being
-                                yes</p>
+                        <div class="col">
+                            <h5 class="mb-1">Segement 1</h5>
+                            <p class="text-color-secondary">This segment has the highest likelihood of subscription being yes</p>
                         </div>
                     </div>
                     <div class="grid justify-content-between">
                         <div class="col m-2">
                             <p class="text-lg">Outcome likelihood:</p>
-                            <h1 class="ml-2">88%</h1>
+                            <h3 class="ml-2">88%</h3>
                         </div>
-                        <div class=" col-8 justify-content-end">
+                        <div class="col-9 justify-content-end">
                             <DataTable :value="products" stripedRows table-style="justify-content-center">
                                 <Column field="field" header="Field"></Column>
                                 <Column field="value" header="Value"></Column>
-
                             </DataTable>
                         </div>
                     </div>
                 </div>
-                <div class="card col ">
-                    <div class="grid justify-content-between">
-                        <div class="col-1 mr-3">
-                            <Avatar :style="{ backgroundColor: randomColor(), color: randomColor() }" size="xlarge"
-                                shape="circle" icon="pi pi-star-fill" />
+                <div class="card col">
+                    <div class="grid justify-content-between gap-1">
+                        <div class="col-1">
+                            <Avatar :style="{ backgroundColor: randomColor(), color: randomColor() }" size="large" shape="circle" icon="pi pi-star-fill" />
                         </div>
-                        <div class="col m-2">
-                            <h5>Segement 1</h5>
-                            <p class="text-color-secondary   ">This segment has the highest likelihood of subscription being
-                                yes</p>
+                        <div class="col">
+                            <h5 class="mb-1">Segement 1</h5>
+                            <p class="text-color-secondary">This segment has the highest likelihood of subscription being yes</p>
                         </div>
                     </div>
                     <div class="grid justify-content-between">
                         <div class="col m-2">
                             <p class="text-lg">Outcome likelihood:</p>
-                            <h2 class="ml-2">88%</h2>
+                            <h4 class="ml-2">88%</h4>
                         </div>
-                        <div class=" col-8 justify-content-end">
+                        <div class="col-9 justify-content-end">
                             <DataTable :value="products" stripedRows table-style="justify-content-center">
                                 <Column field="field" header="Field"></Column>
                                 <Column field="value" header="Value"></Column>
-
                             </DataTable>
                         </div>
                     </div>
                 </div>
-
-
-
-
             </div>
         </div>
-
-
     </div>
 </template>
-<style lang="scss" >
+<style lang="scss">
 .current-counter {
     font-size: x-large;
 
@@ -244,7 +189,6 @@ function randomColor() {
 }
 
 .p-progressbar-value {
-
     font-size: large;
 }
 
@@ -254,7 +198,7 @@ function randomColor() {
 
 .p-progressbar .p-progressbar-value {
     background-color: rgb(0, 122, 255);
-    border-radius: 6px
+    border-radius: 6px;
 }
 
 .p-avatar-icon {
